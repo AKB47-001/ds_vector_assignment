1529d2127a8b9036a427bffaf7ee5b5b2d511b9f
AKB47-001 <ankit.niit87@gmail.com>
2025-06-25 13:20:16 +0530

    Updating Client.py

--- src/client.py
+++ src/client.py
@@ -19,7 +19,7 @@ def get(node, key):
     print(f"GET from {node}: {res.json()}")
 
 # SIMULATING THE CASUAL SCENARIO
-print("---- Step_1: Node One writes x=A ----")
+print("---- Step_1: Node One writes x=Alpha ----")
 put("node1", "x", "Alpha", {"node1": 1, "node2": 0, "node3": 0}, "node1")
 time.sleep(1)
 
@@ -27,7 +27,7 @@ print("---- Step_2: Node Two reads x ----")
 get("node2", "x")
 time.sleep(1)
 
-print("---- Step_3: Node Two writes x=B ----")
+print("---- Step_3: Node Two writes x=Beta ----")
 put("node2", "x", "Beta", {"node1": 1, "node2": 1, "node3": 0}, "node2")
 time.sleep(1)
 
